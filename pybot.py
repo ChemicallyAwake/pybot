3631d68b5f63de886aaca13e2b9b54f6f382c81b
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-09-01 01:21:00 -0700

    Create pybot.py

---
+++ pybot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+BOT_PREFIX = ("pybot ", "pibot ")
+client = commands.Bot(command_prefix=BOT_PREFIX)
+
+@client.command(name='ping')
+async def pingPong():
+    await client.say("pong")
+
+@client.command(name='server_count')
+async def server_count():
+    count = 0
+    for server in client.servers:
+        for member in server.members:
+            count += 1
+    await client.say("%s members" %count)
+
+client.run(os.getenv('TOKEN'))
