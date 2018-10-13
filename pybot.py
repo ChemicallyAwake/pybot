533c1224f0a38968616a47a8bcbefa2a46b6c580
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-10-13 15:40:13 -0700

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix=BOT_PREFIX)
 
 @client.command(name='ping')
 async def pingPong():
-    await client.say(":ping:")
+    await client.say("<:emoji_name:ping>")
 
 @client.command(name='server_count')
 async def server_count():
