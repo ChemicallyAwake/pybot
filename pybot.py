e80f7a4591b2d1e9da9a914cc4315a024dac7fba
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-10-17 14:15:07 -0700

    Pybot has had a from the ground up update
    + permission system
    + rm command :  deletes messages in 
    + echo command : have pybot say something for you
    
    - server_count command

--- pybot.py
+++ pybot.py
@@ -2,19 +2,78 @@ import discord
 from discord.ext import commands
 import os
 
-BOT_PREFIX = ("pybot ", "pibot ")
-client = commands.Bot(command_prefix=BOT_PREFIX)
+BOT_PREFIX = ("py.")
+bot.remove_command('help')
 
-@client.command(name='ping')
-async def pingPong():
-    await client.say("pong")
+CMDS = [{'command' : 'ping',
+        'description' : 'a way to see if bot is responsive.',
+        },
+        {'command' : 'echo',
+        'description' : 'make the bot say something',
+        'role': 'user++',]
+        },
+        {'command' : 'rm',
+        'description' : 'delete message(s)',
+        'permissions': ['manage_messages',]
+        },
+]
 
-@client.command(name='server_count')
-async def server_count():
-    count = 0
-    for server in client.servers:
-        for member in server.members:
-            count += 1
-    await client.say("%s members" %(count-1))
+async def isAuth(cmd, msg):
+    if cmd.get("permissions") != None or cmd.get("role") != None: # if command has a role or permission requirement : role takes priority
+        if cmd.get('role'): # Checks to see if the requirment is a role
+            for position in range(len(list(msg.server.role_hierarchy))): # Loops through all of the servers roles : position == int(role position in the role_hierarchy)
+                if str(msg.server.role_hierarchy[position]).lower() == cmd.get('role').lower(): # if server role == command role requirment
+                    break
+            if msg.server.role_hierarchy.index(msg.author.top_role) <= position: # if user's int(top role position in role_hierarchy) <= int(command role requirment position)
+                return True
+            return False
+        else:
+            print("test")
+            for permission in cmd.get("permissions"):
+                for userPerm in msg.channel.permissions_for(msg.author):
+                    if permission in userPerm:
+                        if True in userPerm:
+                            print(userPerm)
+                            continue
+                        else:
+                            return False
+    return True
 
-client.run(os.getenv('TOKEN'))
+async def runCMD(cmd, msg):
+    if cmd == "ping":
+        await ping(msg)
+    elif cmd == "echo":
+        await echo(BOT_PREFIX + cmd,msg)
+    elif cmd == "rm":
+        arg = msg.content[len(BOT_PREFIX + cmd + " "):]
+        if arg.isdigit():
+            await rm(msg, int(arg) + 1)
+        else:
+            await bot.send_message(msg.channel, "Invalid use of command")
+
+async def ping(msg):
+    await bot.send_message(msg.channel, "pong")
+
+async def echo(cmd,msg):
+    await bot.delete_message(msg)
+    await bot.send_message(msg.channel, msg.content[len(cmd + " "):])
+
+async def rm(msg, num = 1):
+    await bot.purge_from(msg.channel, limit=num)
+
+
+@bot.event
+async def on_message(msg):
+    if msg.author == bot.user:
+        pass
+    else:
+        for cmd in CMDS:
+            if msg.content.startswith(BOT_PREFIX + cmd.get("command")):
+                break
+        if msg.content.startswith(BOT_PREFIX + cmd.get("command")):
+            if await isAuth(cmd, msg):
+                await runCMD(cmd.get("command"), msg)
+            else:
+                await bot.send_message(msg.channel, "Too much power. You need to level before you can you that move.")
+
+bot.run(os.getenv('TOKEN'))
